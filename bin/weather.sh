025c0f1e2dcc03a539d4866daf2a93a807526af8
addei <47223277+addei@users.noreply.github.com>
2020-05-28 14:04:36 +0300

    first upload

---
+++ bin/weather.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#really small script for checking weather from terminal using wttr.in backend by igor_chubin
+#made by githubuser addei
+#GPL 2.0
+
+declare -a dependencies=("curl")
+city=$1
+
+checkDependencies() { #this function checks if the required dependencies are found 
+    declare -i flag=0
+    for i in "${dependencies[@]}"
+    do
+        if ! type $i >/dev/null ; then
+            if [ $flag -eq 0 ] ; then
+                flag=1
+            fi
+            echo "$i is missing"
+        fi
+    done
+    if [ ! $flag -eq 0 ] ; then
+        echo "You need to have following dependencies installed: ${dependencies[*]}"
+        exit 1
+    fi
+}
+
+getWeather() {
+    curl wttr.in/$city
+}
+
+checkDependencies
+getWeather
+exit
