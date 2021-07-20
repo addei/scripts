5cd070ed0038945fb0112d6b4a3b9b1ba43d9c93
addei <47223277+addei@users.noreply.github.com>
2021-07-20 20:43:42 +0300

    Update toflac.sh to versio 1.0

--- bin/toflac.sh
+++ bin/toflac.sh
@@ -1,18 +1,34 @@
 #!/bin/bash
-cdparanoia -vsQ
-sleep 5
+#made by githubuser addei
+#GPL 2.0
+
+#declare success variable
+declare -i success=0
+
+# analyse cd content
+cdparanoia -vsQ |& tee -a cd_content.log
+
+# retrieves audio tracks
 cdparanoia -Bl
-sleep 5
+for f in *.wav; do sox "$f" "${f%%.wav}.flac" --multi-threaded; done
+sleep 2
 
-mkdir converted
-for f in *.wav; do sox "$f" "converted/${f%%.wav}.flac" --multi-threaded; done 
-sleep 5
-
-# test quality of the wav -> to flac conversion
+# test quality of the wav -> flac conversion
 for f in *.wav; do ffmpeg -loglevel error -i "$f" -map 0 -f hash - >> wavhash.txt; done
-for f in converted/*.flac; do ffmpeg -loglevel error -i "$f" -map 0 -f hash - >> converted/flachash.txt; done
+for f in *.flac; do ffmpeg -loglevel error -i "$f" -map 0 -f hash - >> flachash.txt; done
 
 # compare hashes
-cmp --silent wavhash.txt converted/flachash.txt || echo "files are different" > FILESAREDIFFERENT.txt
+cmp --silent wavhash.txt flachash.txt || success=1
 
+# remove .wav and .txt files if hashes match
+[ $success -eq 0 ] && for f in *.wav; do rm "$f"; done || echo "files are different" > FILESAREDIFFERENT.txt
+[ $success -eq 0 ] && for f in *.txt; do rm "$f"; done || echo "check out hash content"
+
+# echo
+[ $success -eq 0 ] && echo "All good, ejecting CD now!" || echo "Some errors occured, ejecting CD now!"
+
+# eject the disc from drive
 eject -T
+
+# start ID3 Tagger
+[ $success -eq 0 ] && kid3 dir "&(pwd)" & disown kid3 ||
