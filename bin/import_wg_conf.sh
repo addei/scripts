74b6eada2d512fe66b31849ead8139963d86e534
addei <47223277+addei@users.noreply.github.com>
2022-05-10 15:09:26 +0300

    Update import_wg_conf.sh
    version 0.2

--- bin/import_wg_conf.sh
+++ bin/import_wg_conf.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 #not so efficient way to import wireguard configuration files from a folder
-#version 0.1 alpha
-
+#version 0.2 alpha
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 declare -a configs
-configs=($(find -maxdepth 1 -name "*.conf"))
+counter=1
+configs=($(ls | grep ".conf"))
 echo ""${#configs[@]}" configuration file(s) found!"
 
 for i in "${configs[@]}"
 do
-    sudo nmcli connection import type wireguard file $i
-    nmcli device disconnect $(echo $i | sed 's/^..//' | cut -d "." -f 1)
+    echo ""$counter"/"${configs[@]}" - "$i""
+    nmcli connection import type wireguard file $i
+    nmcli device disconnect $(echo $i | cut -d "." -f 1)
+    nmcli connection modify $(echo $i | cut -d "." -f 1) connection.autoconnect no
+    nmcli connection modify $(echo $i | cut -d "." -f 1) ipv4.dhcp-send-hostname no
+    nmcli connection modify $(echo $i | cut -d "." -f 1) ipv6.dhcp-send-hostname no
+    ((counter++))
 done
